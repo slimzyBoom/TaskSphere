c0539fab4f1ae7a0fb230e6cd4a08309838296c3
Freddie6578 <newm6578@gmail.com>
2025-01-09 11:02:07 +0100

    Created Sidebar Layout

--- src/main.js
+++ src/main.js
@@ -5,13 +5,14 @@ import App from './App.vue'
 
 // Font Awesome Imports
 import { library } from '@fortawesome/fontawesome-svg-core';
-import {faUser, faLock, faEnvelope} from '@fortawesome/free-solid-svg-icons'; // Import icons you need
+import {fas} from '@fortawesome/free-solid-svg-icons'; 
+import {far} from '@fortawesome/free-regular-svg-icons'; 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 
 // Add icons to the library
-library.add(faUser, faLock, faGoogle, faEnvelope);
+library.add(far, fas, fab);
 
 createApp(App)
 .use(router)
